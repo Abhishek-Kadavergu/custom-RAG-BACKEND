7195aee842874edb00cd1bae36b8165df28687eb
Abhishek Kadavergu <159815044+Abhishek-Kadavergu@users.noreply.github.com>
2026-07-15 11:35:48 +0530

    Refactor middleware and update comments in main.py

--- main.py
+++ main.py
@@ -8,30 +8,29 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# Initialize app
+
 app = FastAPI()
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict to your frontend URL
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# 🔐 Put your Groq API key here
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
 
 
-# Load RAG pipeline once (important 🚨)
+
 rag = RAGPipeline(api_key=GROQ_API_KEY)
 
 
-# Request schema
+
 class QueryRequest(BaseModel):
     question: str
 
